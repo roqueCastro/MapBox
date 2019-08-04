5cf883f5608beec21025ad1a41feced659da57b2
alien <rokke1558@gmail.com>
2019-08-03 23:51:30 -0500

    Actualizar marcadores y actualizar ruta para consumir

--- app/src/main/java/com/example/mapbox/RutaSerpiente.java
+++ app/src/main/java/com/example/mapbox/RutaSerpiente.java
@@ -12,6 +12,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.util.SortedList;
+import android.view.View;
 import android.widget.Toast;
 /*
 import com.mapbox.api.directions.v5.DirectionsCriteria;
@@ -31,13 +32,18 @@ import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.LineString;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.plugins.markerview.MarkerView;
+import com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager;
 import com.mapbox.mapboxsdk.style.layers.LineLayer;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.mapboxsdk.utils.BitmapUtils;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -58,8 +64,11 @@ import retrofit2.Response;
 import timber.log.Timber;
 
 import static com.mapbox.api.directions.v5.DirectionsCriteria.GEOMETRY_POLYLINE;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.get;
 import static com.mapbox.mapboxsdk.style.layers.Property.LINE_CAP_ROUND;
 import static com.mapbox.mapboxsdk.style.layers.Property.LINE_JOIN_ROUND;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineCap;
@@ -67,8 +76,12 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineJoin;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineOpacity;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textField;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textOffset;
 
-public class RutaSerpiente extends AppCompatActivity implements OnMapReadyCallback {
+public class RutaSerpiente extends AppCompatActivity implements OnMapReadyCallback, MapboxMap.OnMapClickListener {
 
 
     private static final float NAVIGATION_LINE_WIDTH = 6;
@@ -77,17 +90,26 @@ public class RutaSerpiente extends AppCompatActivity implements OnMapReadyCallba
     private static final String DRIVING_ROUTE_POLYLINE_SOURCE_ID = "DRIVING_ROUTE_POLYLINE_SOURCE_ID";
     private static final int DRAW_SPEED_MILLISECONDS = 500;
 
+
+    private static final String PROFILE_NAME = "PROFILE_NAME";
+    private static final String ORIGEN = "ORIGIN";
+    private static final String DESTINO = "DESTINATION";
+
     // Origin point in Paris, France
-    private static final Point PARIS_ORIGIN_POINT = Point.fromLngLat(-76.048722, 1.852760);
+    private Point PARIS_ORIGIN_POINT = Point.fromLngLat(-76.048722, 1.852760);
 
     // Destination point in Lyon, France
     private static final Point LYON_DESTINATION_POINT = Point.fromLngLat(-75.932837, 1.971442);
+    private static final String SOURCE_ID = "source-id";
+    private  GeoJsonSource jsonSource;
 
     private MapView mapView;
     private MapboxMap mapboxMap;
     private MapboxDirections mapboxDirectionsClient;
     private Handler handler = new Handler();
     private Runnable runnable;
+
+
 
 
     @Override
@@ -111,41 +133,115 @@ public class RutaSerpiente extends AppCompatActivity implements OnMapReadyCallba
     public void onMapReady(@NonNull MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
 
-        Drawable image  = ContextCompat.getDrawable(getApplicationContext(), R.drawable.red_marker);
-        Bitmap bitmap =drawableToBitmap(image);
+
+
 
         this.mapboxMap.setStyle(new Style.Builder().fromUri(Style.LIGHT)
-                .withImage("icon-id", bitmap)
-                .withSource(new GeoJsonSource("source-id",
-                        FeatureCollection.fromFeatures(new Feature[] {
-                                Feature.fromGeometry(Point.fromLngLat(PARIS_ORIGIN_POINT.longitude(), PARIS_ORIGIN_POINT.latitude())),
-                                Feature.fromGeometry(Point.fromLngLat(LYON_DESTINATION_POINT.longitude(), LYON_DESTINATION_POINT.latitude())),
-                        })))
-                .withLayer(new SymbolLayer("layer-id",
-                        "source-id").withProperties(
-                        iconImage("icon-id"),
-                        iconOffset(new Float[] {0f, -8f})
-                ))
-
-// Add a source and LineLayer for the snaking directions route line
-                .withSource(new GeoJsonSource(DRIVING_ROUTE_POLYLINE_SOURCE_ID))
-                .withLayerBelow(new LineLayer(DRIVING_ROUTE_POLYLINE_LINE_LAYER_ID,
-                        DRIVING_ROUTE_POLYLINE_SOURCE_ID)
-                        .withProperties(
-                                lineWidth(NAVIGATION_LINE_WIDTH),
-                                lineOpacity(NAVIGATION_LINE_OPACITY),
-                                lineCap(LINE_CAP_ROUND),
-                                lineJoin(LINE_JOIN_ROUND),
-                                lineColor(Color.parseColor("#d742f4"))
-                        ), "layer-id"), new Style.OnStyleLoaded() {
+                , new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
-                getDirectionsRoute(PARIS_ORIGIN_POINT, LYON_DESTINATION_POINT);
+                mapboxMap.addOnMapClickListener(RutaSerpiente.this);
+                inicioMarkers(style);
+                //getDirectionsRoute(PARIS_ORIGIN_POINT, LYON_DESTINATION_POINT);
             }
         });
 
     }
 
+    private void inicioMarkers(Style style) {
+
+        Drawable image  = ContextCompat.getDrawable(getApplicationContext(), R.drawable.red_marker);
+        Bitmap bitmap =drawableToBitmap(image);
+
+
+        Feature origFeature = Feature.fromGeometry(Point.fromLngLat(PARIS_ORIGIN_POINT.longitude(),
+                PARIS_ORIGIN_POINT.latitude()));
+        origFeature.addStringProperty(PROFILE_NAME, ORIGEN);
+
+        Feature destiFeature = Feature.fromGeometry(Point.fromLngLat(LYON_DESTINATION_POINT.longitude(),
+                LYON_DESTINATION_POINT.latitude()));
+        destiFeature.addStringProperty(PROFILE_NAME, DESTINO);
+
+        jsonSource = new GeoJsonSource(SOURCE_ID,
+                FeatureCollection.fromFeatures(new Feature[] {
+                        origFeature,
+                        destiFeature,
+                }));
+
+        style.addSource(jsonSource);
+        // Add a source and LineLayer for the snaking directions route line
+        style.addSource(new GeoJsonSource(DRIVING_ROUTE_POLYLINE_SOURCE_ID));
+
+        style.addLayerBelow(new LineLayer(DRIVING_ROUTE_POLYLINE_LINE_LAYER_ID,
+                DRIVING_ROUTE_POLYLINE_SOURCE_ID)
+                .withProperties(
+                        lineWidth(NAVIGATION_LINE_WIDTH),
+                        lineOpacity(NAVIGATION_LINE_OPACITY),
+                        lineCap(LINE_CAP_ROUND),
+                        lineJoin(LINE_JOIN_ROUND),
+                        lineColor(Color.parseColor("#d742f4"))
+                ), "layer-id");
+
+        style.addLayer(new SymbolLayer("layer_id", SOURCE_ID).withProperties(
+                iconImage(get(PROFILE_NAME)),
+                iconIgnorePlacement(true),
+                iconAllowOverlap(true),
+                textField(get(PROFILE_NAME)),
+                textIgnorePlacement(true),
+                textAllowOverlap(true),
+                textOffset(new Float[] {0f, 2f})
+        ));
+        mapView.addOnStyleImageMissingListener(new MapView.OnStyleImageMissingListener() {
+            @Override
+            public void onStyleImageMissing(@NonNull String id) {
+                switch (id) {
+                    case ORIGEN:
+                        addImage(id, R.drawable.ic_car);
+                        break;
+                    case DESTINO:
+                        addImage(id, R.drawable.red_marker);
+                        break;
+                }
+            }
+        });
+        LatLng position = new LatLng(PARIS_ORIGIN_POINT.latitude(),PARIS_ORIGIN_POINT.longitude());
+        mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 10));
+        getDirectionsRoute(PARIS_ORIGIN_POINT,LYON_DESTINATION_POINT);
+    }
+
+    private void addImage(String id, int drawableImage) {
+        Style style = mapboxMap.getStyle();
+        if (style != null) {
+            style.addImageAsync(id, BitmapUtils.getBitmapFromDrawable(
+                    getResources().getDrawable(drawableImage)));
+        }
+    }
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+
+        PARIS_ORIGIN_POINT = Point.fromLngLat(point.getLongitude(),point.getLatitude());
+
+        Feature origFeature = Feature.fromGeometry(Point.fromLngLat(PARIS_ORIGIN_POINT.longitude(),
+                PARIS_ORIGIN_POINT.latitude()));
+        origFeature.addStringProperty(PROFILE_NAME, ORIGEN);
+
+        Feature destiFeature = Feature.fromGeometry(Point.fromLngLat(LYON_DESTINATION_POINT.longitude(),
+                LYON_DESTINATION_POINT.latitude()));
+        destiFeature.addStringProperty(PROFILE_NAME, DESTINO);
+
+
+        if (mapboxMap.getStyle() != null) {
+            jsonSource = mapboxMap.getStyle().getSourceAs(SOURCE_ID);
+            if (jsonSource != null) {
+                jsonSource.setGeoJson(FeatureCollection.fromFeatures(new Feature[]{origFeature,destiFeature}
+                ));
+            }
+        }
+        getDirectionsRoute(PARIS_ORIGIN_POINT,LYON_DESTINATION_POINT);
+
+        return true;
+    }
 
 
     public static Bitmap drawableToBitmap (Drawable drawable) {
@@ -200,10 +296,10 @@ public class RutaSerpiente extends AppCompatActivity implements OnMapReadyCallba
                     return;
                 }
 
-// Get the route from the Mapbox Directions API response
+                // Get the route from the Mapbox Directions API response
                 DirectionsRoute currentRoute = response.body().routes().get(0);
 
-// Start the step-by-step process of drawing the route
+                // Start the step-by-step process of drawing the route
                 runnable = new DrawRouteRunnable(mapboxMap, currentRoute.legs().get(0).steps(), handler);
                 handler.postDelayed(runnable, DRAW_SPEED_MILLISECONDS);
             }
